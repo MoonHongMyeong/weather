d6432be567e4bb62d499c314175f2a6128cea3c6
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-11-29 10:37:56 +0900

    Add: phase2 Mybatis 설정 [#1]

---
+++ src/main/java/com/portfolio/weather/config/MybatisConfig.java
@@ -0,0 +1,45 @@
+package com.portfolio.weather.config;
+
+import org.apache.ibatis.logging.stdout.StdOutImpl;
+import org.apache.ibatis.session.AutoMappingBehavior;
+import org.apache.ibatis.session.ExecutorType;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.type.JdbcType;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("com.portfolio.weather.mapper")
+public class MybatisConfig {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        sessionFactory.setMapperLocations(
+            new PathMatchingResourcePatternResolver().getResources("classpath:mapper/*.xml")
+        );
+
+        org.apache.ibatis.session.Configuration configuration =
+                new org.apache.ibatis.session.Configuration();
+        configuration.setMapUnderscoreToCamelCase(true);
+        configuration.setJdbcTypeForNull(JdbcType.NULL);
+        configuration.setCallSettersOnNulls(true);
+        configuration.setCacheEnabled(true);
+        configuration.setDefaultStatementTimeout(30);
+        configuration.setMultipleResultSetsEnabled(true);
+        configuration.setUseGeneratedKeys(true);
+        configuration.setAutoMappingBehavior(AutoMappingBehavior.PARTIAL);
+        configuration.setDefaultExecutorType(ExecutorType.REUSE);
+        configuration.setLogImpl(StdOutImpl.class);
+
+        sessionFactory.setConfiguration(configuration);
+        
+        return sessionFactory.getObject();
+    }
+}
