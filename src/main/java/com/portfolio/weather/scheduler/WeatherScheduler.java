e85e6e80b6606ba3ab68ca37d0a1ca4cc339e19c
MoonHongMyeong <moonhongmyeong@gmail.com>
2024-12-16 22:54:07 +0900

    Modify: 단기예보 스케줄러 크론 시간 업데이트 되면 바로 받을 수 있도록 수정

--- src/main/java/com/portfolio/weather/scheduler/WeatherScheduler.java
+++ src/main/java/com/portfolio/weather/scheduler/WeatherScheduler.java
@@ -25,7 +25,7 @@ public class WeatherScheduler {
      * - 발표시각 02:00, 05:00, 08:00, 11:00, 14:00, 17:00, 20:00, 23:00 (1일 8회)
      * - API 제공 시간(~이후) : 02:10, 05:10, 08:10, 11:10, 14:10, 17:10, 20:10, 23:10
      * */ 
-    @Scheduled(cron = "0 13 */3 * * *")
+    @Scheduled(cron = "0 13 2/3 * * *")
     public void executeVillageForecast(){
         // 대전시 모든 좌표에 대해 예보 조회
         for (DaejeonCoordinate coord : DaejeonCoordinate.values()) {
